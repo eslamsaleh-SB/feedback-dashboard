12051ec38a8d58c3e743b1725c4cf08163f2dee9
eslamsaleh-SB <eslam.saleh@hudl.com>
2026-06-15 08:56:37 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     // Allow Server Actions / route bodies up to 20MB (default is 1MB).
     serverActions: { bodySizeLimit: "20mb" },
   },
+  // Pilot: don't fail the production build on type/lint warnings.
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
 
 module.exports = nextConfig;
